d46e5d91dd68c09c6d9da7ad6ebf26749f5cbec1
Caio Cabral <caiocabral.ep@gmail.com>
2026-01-04 19:32:53 -0300

    feat: :sparkles: Início do treinamento

---
+++ Welcome/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Welcome
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bem Vindo ao Treinando C# na Dio!");
+        }
+    }
+}
